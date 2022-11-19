2df5584f0bd9026bd000c514bf48d50464c48375
MD ZIAUDDIN <md.ziauddin02@gmail.com>
2022-11-19 12:02:40 +0530

    adds dot env

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors';
 
 import Campaignoutes from './routes/campaigns.js'
+
 
 const app = express();
 
@@ -15,7 +19,7 @@ app.use(bodyParser.urlencoded({ "limit": "30mb", extended: true }))
 app.use(cors())
 
 // DB connection
-const CONNECTION_URL = 'mongodb+srv://zia02:fundMundiSecret@fund-mundi-cluster.hpfqsft.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = `mongodb+srv://zia02:${process.env.DB_PASSWORD}@fund-mundi-cluster.hpfqsft.mongodb.net/`
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))).catch((err) => console.log(err.message))
